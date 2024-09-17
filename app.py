b82f4b9e40a1faf57fa87cbc9227351ec04aa5d2
Valery Baranov <v.baranov@integer-soft.ru>
2024-09-18 00:39:12 +0300

    init

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/<name>")
+def escape(name):
+    return f"Hello, {escape(name)}!"
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', person=name)
